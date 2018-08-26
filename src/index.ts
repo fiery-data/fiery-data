bc4008b94714136a808b4b742e3be3cccb0036be
Philip Diffenderfer <pdiffenderfer@gmail.com>
2018-08-26 13:54:38 -0400

    Release 0.0.9

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { define, setGlobalOptions, mergeStrategy, mergeOptions } from './options
 import { getCacheForData, destroyCache } from './cache'
 import { getInstance } from './instance'
 
+export * from './constants'
+export * from './types'
 export { define, setGlobalOptions, mergeStrategy, mergeOptions }
 export { getCacheForData, destroyCache }
 export default getInstance
